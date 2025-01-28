213cd1db949014bcf8c5a43e21fd57c785e320c6
Yi Lei Feng <fengyilei1@gmail.com>
2025-01-28 14:23:21 -0500

    save work for chart customization

--- src/components/config-customization.vue
+++ src/components/config-customization.vue
@@ -1,9 +1,23 @@
 <template>
-    <div class="">
-        This is the config customization section for highcharts editor
+    <div class="chart-customization m-6">
+        <div class="text-2xl font-bold">{{ $t('editor.customization.title') }}</div>
     </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed, ref, onMounted } from 'vue';
+import { useChartStore } from '../stores/chartStore';
+import { useDataStore } from '../stores/dataStore';
 
-<style lang="scss"></style>
+import Highcharts from 'highcharts';
+import dataModule from 'highcharts/modules/data';
+
+dataModule(Highcharts);
+
+</script>
+
+<style lang="scss">
+.dv-chart-container {
+    width: 98%;
+}
+</style>
